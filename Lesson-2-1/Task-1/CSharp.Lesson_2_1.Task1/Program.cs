bc77fde08de019e77bb7912b3b723f2438894d65
Leonid Peterov <ua1wcz@gmail.com>
2021-07-02 18:52:24 +0300

    CSharp.Lesson-2_1.Homework

---
+++ Lesson-2-1/Task-1/CSharp.Lesson_2_1.Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CSharp.Lesson_2_1.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckNumber());
+        }
+
+        private static string CheckNumber()
+        {
+            Console.Write("Введите число: ");
+
+            int number;
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                int d = 0;
+                int i = 2;
+
+                while (i < number)
+                {
+                    if (number % i == 0)
+                    {
+                        d++;
+                    }
+
+                    i++;
+                } 
+                
+                if (d == 0)
+                {
+                   return "Число простое!";
+                }
+                else
+                {
+                    return "Число составное!";
+                }  
+            }
+
+            return "Данные не введены";
+        }
+    }
+}
